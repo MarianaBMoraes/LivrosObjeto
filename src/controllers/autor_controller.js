e0ac4e6dbb9ff174fbc566029d1550c2fcd9619a
MarianaBMoraes <marianabmoraes.ef@gmail.com>
2024-09-12 20:49:09 -0300

    feat

--- src/controllers/autor_controller.js
+++ src/controllers/autor_controller.js
@@ -1,27 +1,27 @@
-const Autor = require("../models/livro_model.js");
+const Autor = require("../models/autor_model.js");
 
 exports.createAutor = async (req, res) => {
   try {
-    const livro = await Autor.create(req.body);
-    res.status(201).json(livro);
+    const autor = await Autor.create(req.body);
+    res.status(201).json(autor);
   } catch (error) {
-    res.status(400).json({ error: "Erro ao criar livro!" });
+    res.status(400).json({ error: "Erro ao criar autor!" });
   }
 };
 
 exports.getAutor = async (req, res) => {
   try {
-    const livros = await Autor.find();
-    res.status(200).json(livros);
+    const autor = await Autor.find();
+    res.status(200).json(autor);
   } catch (error) {
-    res.status(400).json({ error: "Erro ao buscar livros!" });
+    res.status(400).json({ error: "Erro ao buscar autor!" });
   }
 };
 
 exports.getAutorById = async (req, res) => {
   try {
-    const livro = await Autor.findById(req.params.id);
-    res.status(200).json(livro);
+    const autor = await Autor.findById(req.params.id);
+    res.status(200).json(autor);
   } catch (error) {
     res.status(404).json({ error: "Autor não encontrado!" });
   }
@@ -29,12 +29,12 @@ exports.getAutorById = async (req, res) => {
 
 exports.updateAutor = async (req, res) => {
   try {
-    const livro = await Auotr.findByIdAndUpdate(req.params.id, req.body, {
+    const autor = await Auotr.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
     });
-    res.status(200).json(livro);
+    res.status(200).json(autor);
   } catch (error) {
-    res.status(400).json({ error: "Erro ao atualizar livro!" });
+    res.status(400).json({ error: "Erro ao atualizar autor!" });
   }
 };
 
@@ -43,6 +43,6 @@ exports.deleteAutor = async (req, res) => {
     await Autor.findByIdAndDelete(req.params.id);
     res.status(204).json();
   } catch (error) {
-    res.status(400).json({ error: "Erro ao deletar livro!" });
+    res.status(400).json({ error: "Erro ao deletar autor!" });
   }
 };
